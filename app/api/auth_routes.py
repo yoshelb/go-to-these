620e4632951ad903dbc6b3f9037b0a0cd47c26a0
Shelbey Casalena <shelbey.casalena@gmail.com>
2024-06-12 17:24:49 -0700

    hopefully fixed o-auth

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -35,19 +35,29 @@ Some of these values will come from our .env file.
 # Import our credentials from the .env file
 CLIENT_SECRET = os.getenv('GOOGLE_OAUTH_CLIENT_SECRET')
 CLIENT_ID = os.getenv('GOOGLE_OAUTH_CLIENT_ID')
+PROD_REDIRECT_URI = os.getenv('PROD_REDIRECT_URI')
+
+# Determine the environment
+ENV = os.getenv('FLASK_ENV', 'development')
+
+# Set the redirect URIs based on the environment
+if ENV == 'production':
+    REDIRECT_URI = 'https://gotothese.onrender.com/api/auth/callback'
+else:
+    REDIRECT_URI = 'http://localhost:8000/api/auth/callback'
 
 # The dictionary to be written out as JSON
 client_secrets = {
-  "web": {
-    "client_id": CLIENT_ID,
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_secret": CLIENT_SECRET,
-    "redirect_uris": [
-      "http://localhost:8000/api/auth/callback"
-    ]
-  }
+    "web": {
+        "client_id": CLIENT_ID,
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_secret": CLIENT_SECRET,
+        "redirect_uris": [
+            REDIRECT_URI
+        ]
+    }
 }
 
 """
@@ -78,7 +88,7 @@ flow = google_auth_oauthlib.flow.Flow(
     oauth2_session,
     client_type='web',
     client_config=client_config,
-    redirect_uri="http://localhost:8000/api/auth/callback",
+    redirect_uri=REDIRECT_URI,
 )
 
 ############ END OAUTH 2.0 SETUP ##########################################################################

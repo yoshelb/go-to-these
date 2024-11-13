c411c70d2098ee9d1e6c093426ec2d218ad3bce9
Shelbey Casalena <shelbey.casalena@gmail.com>
2024-11-13 00:26:55 -0800

    added script to update photos

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,12 @@ from .seeds import seed_commands
 from .config import Config
 from .api.list_routes import list_routes
 from sqlalchemy.orm import joinedload
+from .models.place import Place
+import requests
+import os
+
+API_KEY = os.environ.get('GOOGLE_API_KEY')
+
 
 template_folder_path = os.path.join(os.path.dirname(__file__), 'templates')
 print(f"Template folder path: {template_folder_path}")
@@ -23,7 +29,6 @@ print(f"List of templates: {os.listdir(template_folder_path)}")
 # Setup login manager
 login = LoginManager(app)
 login.login_view = 'auth.unauthorized'
-
 
 @login.user_loader
 def load_user(id):
@@ -43,6 +48,20 @@ Migrate(app, db)
 
 # Application Security
 CORS(app)
+def fetch_place_photo(place_id):
+    url = f"https://places.googleapis.com/v1/places/{place_id}?fields=photos&key={API_KEY}"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        photo = data.get("photos")[0].get("name")
+        # print(photo)
+        photo_uri = f"https://places.googleapis.com/v1/{photo}/media?maxWidthPx=800&maxHeightPx=800&key={API_KEY}"
+
+        return photo_uri
+    else:
+        print(f"Error: {response.status_code}")
+        return None
 
 
 # Since we are deploying with Docker and Flask,
@@ -50,6 +69,31 @@ CORS(app)
 # Therefore, we need to make sure that in production any
 # request made over http is redirected to https.
 # Well.........
+@app.cli.command("run-script")
+def run_script():
+    # Place your script logic here
+    print("Running custom script...")
+    # Example: accessing the database or doing some task
+    # db.session.add(...) or any other operations
+
+    places = Place.query.all()
+    for place in places:
+        place_data = place.to_dict()
+        # previewImage = place_data.get("previewImage")
+        # try:
+        #     response = requests.get(previewImage)
+        #     if response.status_code == 400:
+        #         print(f"Image URL for Place {place.id} returned 400. Updating image...")
+        #         # Update image logic here (e.g., setting a default image)
+        #         place.previewImage = "https://cdn.pixabay.com/photo/2024/01/31/10/02/kitten-8543762_960_720.jpg"  # Set a fallback image URL
+        new_preview_image = fetch_place_photo(place_data.get("id"))
+        place.previewImage = new_preview_image
+        db.session.commit()  # Save the changes to the database
+        # except requests.exceptions.RequestException as e:
+        #     print(f"Error accessing image URL for Place {place.id}: {e}")
+
+
+
 @app.before_request
 def https_redirect():
     if os.environ.get('FLASK_ENV') == 'production':
